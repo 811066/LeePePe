1d63576049f9404efc8706c92753d0674fcff5b5
811066 <90673258+811066@users.noreply.github.com>
2022-01-19 14:34:17 +0800

    Update fetch_input.c

--- fetch_input.c
+++ fetch_input.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
+#包括 < stdio.h >
 
-int main() {
+詮釋 主要（）{
 
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
-    int e = 0;
+    int a = 0 ;
+    詮釋b = 0 ;
+    詮釋c = 0 ;
+    詮釋d = 0 ;
+    整數e = 0 ;
 
     printf("請輸入五個數字，以空白隔開。\n");
-    //add scanf code to get data from input
-    //and feed those input into a, b, c, d, e, respectively.
-    //only ONE line code is you need
-    //please refer page 18 of slides
-    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
-    printf("a=%d\n", a);
-    printf("b=%d\n", b);
-    printf("c=%d\n", c);
-    printf("d=%d\n", d);
-    printf("e=%d\n", e);
-    printf("請按任意鍵繼續 . . .");
+    //添加scanf代碼以從輸入中獲取數據
+    //並將這些輸入分別輸入到 a、b、c、d、e 中。
+    //你只需要一行代碼
+    //請參考第 18 頁的幻燈片
+scanf ( " %d%d%d%d%d " , &a, &b, &c, &d, &e);
+    printf ( " a= %d \n " , a);
+    printf ( " b= %d \n " , b);
+    printf ( " c= %d \n " , c);
+    printf ( " d= %d \n " , d);
+    printf ( " e= %d \n " , e);
 }
-© 2021 GitHub, Inc.
-Terms
-Pr
